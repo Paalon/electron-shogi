f434f179478b4e4121f159c64338613993e6aac0
Koki Fushimi <paalon1936@gmail.com>
2022-12-20 10:30:24 +0900

    Add comments.

--- src/shogi/book.ts
+++ src/shogi/book.ts
@@ -2,27 +2,52 @@ import Graph from "graphology";
 import { Position } from "./position";
 import { Move, parseUSIMove } from "./move";
 
+/**
+ * 定跡を表すクラス
+ */
 export class Book {
+  // SFEN をノードの ID として使用する。
   public graph = new Graph({
     multi: false,
     allowSelfLoops: false,
-    type: "directed"
+    type: "directed",
   });
-  constructor() {}
+  // constructor() {}
+
+  /**
+   * SFEN から局面を追加する。
+   */
   addPositionBySFEN(sfen: string) {
     this.graph.addNode(sfen);
-  };
+  }
+  
+  /**
+   * SFEN で指定された局面とその局面に接続する指し手を削除する。
+   */
   removePositionBySFEN(sfen: string) {
     this.graph.dropNode(sfen);
   }
+  
+  /**
+   * 局面を追加する。
+   * 既に同じ局面が存在するときは何もしない。
+   */
   addPosition(position: Position) {
     const sfen = position.sfen;
     this.addPositionBySFEN(sfen);
   }
+  
+  /**
+   * 局面を削除する。
+   */
   removePosition(position: Position) {
     const sfen = position.sfen;
     this.removePositionBySFEN(sfen);
   }
+
+  /**
+   * USI 形式で指定された局面と指し手から指し手を追加する。
+   */
   addMoveByUSI(sfenString: string, usiMoveString: string) {
     const sourceSfen = sfenString;
     const position = Position.newBySFEN(sourceSfen);
@@ -40,13 +65,21 @@ export class Book {
     this.addMove(position, move);
     return true;
   }
+
+  /**
+   * 指し手を追加する。
+   */
   addMove(position: Position, move: Move) {
     const sourceSfen = position.sfen;
     const targetPosition = position.clone();
-    targetPosition.doMove(move, {ignoreValidation: false});
+    targetPosition.doMove(move, { ignoreValidation: false });
     const targetSfen = targetPosition.sfen;
-    this.graph.addEdge(sourceSfen, targetSfen, {move: move.usi, weight: 1});
+    this.graph.addEdge(sourceSfen, targetSfen, { usi: move.usi, weight: 1 });
   }
+
+  /**
+   * 指し手を削除する。
+   */
   removeMove(position: Position, move: Move) {
     const sourceSfen = position.sfen;
     const targetPosition = position.clone();
@@ -54,9 +87,14 @@ export class Book {
     const targetSfen = targetPosition.sfen;
     this.graph.dropEdge(sourceSfen, targetSfen);
   }
+
+  /**
+   * 複製する。
+   */
   copy(): Book {
-    const book = new Book()
+    const book = new Book();
     book.graph = this.graph.copy();
     return book;
   }
+
 }
--- src/tests/shogi/book.spec.ts
+++ src/tests/shogi/book.spec.ts
@@ -1,16 +1,16 @@
-import {
-  Book,
-  Position,
-  Move
-} from "@/shogi";
+import { Book, Position, Move } from "@/shogi";
 
 describe("shogi/book", () => {
   it("Book", () => {
     const book = new Book();
-    const position1 = Position.newBySFEN("lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1") as Position;
-    const position2 = Position.newBySFEN("lnsgkgsnl/1r5b1/ppppppppp/9/9/7P1/PPPPPPP1P/1B5R1/LNSGKGSNL w - 2") as Position;
-    const move1 = position1.createMoveBySFEN("2g2f") as Move;
-    
+    const position1 = Position.newBySFEN(
+      "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1"
+    ) as Position;
+    const position2 = Position.newBySFEN(
+      "lnsgkgsnl/1r5b1/ppppppppp/9/9/7P1/PPPPPPP1P/1B5R1/LNSGKGSNL w - 2"
+    ) as Position;
+    const move1 = position1.createMoveByUSI("2g2f") as Move;
+
     expect(book.graph.order).toBe(0);
     expect(book.graph.size).toBe(0);
     book.addPosition(position1);
