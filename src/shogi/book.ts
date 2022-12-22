812cb6e7511a627c59669c51854bc7e807deadc3
Koki Fushimi <paalon1936@gmail.com>
2022-12-22 13:42:25 +0900

    Implement Book.

--- src/shogi/book.ts
+++ src/shogi/book.ts
@@ -3,98 +3,130 @@ import { Position } from "./position";
 import { Move, parseUSIMove } from "./move";
 
 /**
+ * SFEN から手数を消した文字列を返す。
+ */
+function removePly(sfen: string) {
+  return sfen.split(" ").slice(0, 4).join(" ");
+}
+
+/**
  * 定跡を表すクラス
  */
 export class Book {
-  // SFEN をノードの ID として使用する。
+  // SFEN の手数抜きを graphology の Graph のノードの id として使用する。
   public graph = new Graph({
     multi: false,
     allowSelfLoops: false,
     type: "directed",
   });
-  // constructor() {}
+
+  /**
+   * 局面を追加する。
+   */
+  addPosition(position: Position) {
+    this.addPositionByUSI(position.sfen);
+  }
 
   /**
    * SFEN から局面を追加する。
    */
-  addPositionBySFEN(sfen: string) {
-    this.graph.addNode(sfen);
+  addPositionByUSI(sfen: string) {
+    this.graph.mergeNode(removePly(sfen));
   }
-  
-  /**
-   * SFEN で指定された局面とその局面に接続する指し手を削除する。
-   */
-  removePositionBySFEN(sfen: string) {
-    this.graph.dropNode(sfen);
-  }
-  
-  /**
-   * 局面を追加する。
-   * 既に同じ局面が存在するときは何もしない。
-   */
-  addPosition(position: Position) {
-    const sfen = position.sfen;
-    this.addPositionBySFEN(sfen);
-  }
-  
+
   /**
    * 局面を削除する。
    */
   removePosition(position: Position) {
-    const sfen = position.sfen;
-    this.removePositionBySFEN(sfen);
+    this.removePositionByUSI(position.sfen);
   }
 
   /**
-   * USI 形式で指定された局面と指し手から指し手を追加する。
+   * SFEN で指定された局面とその局面に接続する指し手を削除する。
    */
-  addMoveByUSI(sfenString: string, usiMoveString: string) {
-    const sourceSfen = sfenString;
-    const position = Position.newBySFEN(sourceSfen);
-    if (position === null) {
-      return false;
-    }
-    const parsed = parseUSIMove(usiMoveString);
-    if (parsed === null) {
-      return false;
-    }
-    const move = position.createMove(parsed.from, parsed.to);
-    if (move === null) {
-      return false;
-    }
-    this.addMove(position, move);
-    return true;
+  removePositionByUSI(sfen: string) {
+    this.graph.dropNode(removePly(sfen));
   }
 
   /**
    * 指し手を追加する。
    */
   addMove(position: Position, move: Move) {
-    const sourceSfen = position.sfen;
-    const targetPosition = position.clone();
-    targetPosition.doMove(move, { ignoreValidation: false });
-    const targetSfen = targetPosition.sfen;
-    this.graph.addEdge(sourceSfen, targetSfen, { usi: move.usi, weight: 1 });
+    const position0 = position;
+    const sfen0 = position0.sfen;
+    const position1 = position.clone();
+    position1.doMove(move, { ignoreValidation: false });
+    const sfen1 = position1.sfen;
+    this.graph.mergeEdge(removePly(sfen0), removePly(sfen1), {
+      usiMove: move.usi,
+    });
+  }
+
+  /**
+   * USI 形式で指定された局面と指し手から指し手を追加する。
+   */
+  addMoveByUSI(sfen: string, usiMove: string) {
+    const sfen0 = sfen;
+    const position0 = Position.newBySFEN(sfen);
+    if (position0 === null) {
+      throw "error";
+    }
+    const position1 = position0.clone();
+    const parsed = parseUSIMove(usiMove);
+    if (parsed === null) {
+      throw "error";
+    }
+    const move = position0.createMove(parsed.from, parsed.to);
+    if (move === null) {
+      throw "error";
+    }
+    position1.doMove(move);
+    const sfen1 = position1.sfen;
+    this.graph.mergeEdge(removePly(sfen0), removePly(sfen1), {
+      usiMove: usiMove,
+    });
   }
 
   /**
    * 指し手を削除する。
    */
   removeMove(position: Position, move: Move) {
-    const sourceSfen = position.sfen;
-    const targetPosition = position.clone();
-    targetPosition.doMove(move, { ignoreValidation: false });
-    const targetSfen = targetPosition.sfen;
-    this.graph.dropEdge(sourceSfen, targetSfen);
+    const position0 = position;
+    const sfen0 = position0.sfen;
+    const position1 = position.clone();
+    position1.doMove(move, { ignoreValidation: false });
+    const sfen1 = position1.sfen;
+    this.graph.dropEdge(removePly(sfen0), removePly(sfen1));
   }
 
   /**
    * 複製する。
    */
-  copy(): Book {
+  clone(): Book {
     const book = new Book();
     book.graph = this.graph.copy();
     return book;
   }
 
+  /**
+   * ノード数を返す。
+   */
+  get numberOfNodes(): number {
+    return this.getNumberOfNodes();
+  }
+
+  getNumberOfNodes(): number {
+    return this.graph.order;
+  }
+
+  /**
+   * エッジ数を返す。
+   */
+  get numberOfEdges(): number {
+    return this.getNumberOfEdges();
+  }
+
+  getNumberOfEdges(): number {
+    return this.graph.size;
+  }
 }
